81674d48e424a1c1bf718f94debf9ead61ab7df2
SimonAllenOliver <simonallenoliver@gmail.com>
2024-06-12 17:31:35 -0500

    grab all data complete

--- pollScraper.py
+++ pollScraper.py
@@ -11,7 +11,7 @@ soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 # in this case it is only one row
 
 # finds table at index 1 (second table in soup) and assigns it to var called my_table
-my_table = soup.find('table')
+my_table = soup.find_all("table")[1]
 # print(my_table)
 
 
@@ -29,10 +29,9 @@ my_table = soup.find('table')
 
 # Find all rows within the table
 table_rows = my_table.find_all('tr')
-print(table_rows)
 
 # Iterate through the rows and print the text inside each <td> tag
-# for tr in table_rows:
-#     td = tr.find_all('td')
-#     row = [i.text for i in td]
-#     print(row)
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    print(row)
--- pollScraperV2.py
+++ pollScraperV2.py
@@ -6,7 +6,23 @@ page_to_scrape = requests.get("https://projects.fivethirtyeight.com/polls/presid
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 
 # grabs all dates for most recent polls
-dates = soup.find_all("td", class_="dates")
+dates = soup.find_all("td", class_="dates hide-desktop")
 for date in dates:
     line1 = date.find("div") # extra removed (poll type if want to add later we need 2nd div)
     print(line1.text)
+
+# grabs pollster names
+pollsters = soup.find_all("td", class_="pollster")
+for p in pollsters:
+    print(p.text)
+
+# gets all numbers tds (with names and numbers)
+numbers = soup.find_all("td", class_="answers hide-desktop")
+# gets all biden numbers
+for n in numbers:
+    biden = n.find_all("div")[2]
+    print("Biden", biden.text)
+# gets all trump numbers
+for n in numbers:
+    trump = n.find_all("div")[4]
+    print("Trump", trump.text)
