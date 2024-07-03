8ff79b2750b29e4f8c1861a9ddba62141f0a14d4
SimonAllenOliver <simonallenoliver@gmail.com>
2024-07-03 16:56:07 -0500

    date time now to date now

--- pollScraperV2.py
+++ pollScraperV2.py
@@ -14,9 +14,11 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
+# create var for current date time - may change to just date?
 now = datetime.datetime.now()
 
 sql = "INSERT INTO polls (bidenPercent, trumpPercent, date, pollster, createdAt, updatedAt) VALUES (%s, %s, %s, %s, %s, %s)"
+# these vaues will be our variable from below - this will prob go after the scraping stuff
 val = (49, 51, now, "uGov", now, now)
 mycursor.execute(sql, val)
 
