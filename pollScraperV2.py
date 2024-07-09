31d904305a7d458520abf4b4374bdca96ca454e3
SimonAllenOliver <simonallenoliver@gmail.com>
2024-07-08 20:27:36 -0500

    grouping data in arrays prior to insertion

--- pollScraperV2.py
+++ pollScraperV2.py
@@ -4,26 +4,25 @@ import datetime
 
 # connection to mysql (had to install mysql python connector)
 # connection tested successfully
-import mysql.connector
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="root",
-  database="pollscraperdb"
-)
+# import mysql.connector
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="root",
+#   password="root",
+#   database="pollscraperdb"
+# )
 
-mycursor = mydb.cursor()
+# mycursor = mydb.cursor()
 
-# create var for current date time - may change to just date?
-now = datetime.datetime.now()
+# # create var for current date time - may change to just date?
+# now = datetime.datetime.now()
 
-sql = "INSERT INTO polls (bidenPercent, trumpPercent, date, pollster, createdAt, updatedAt) VALUES (%s, %s, %s, %s, %s, %s)"
-# these vaues will be our variable from below - this will prob go after the scraping stuff
-val = (19, 21, now, "uGov", now, now)
-val = (39, 61, now, "uGov", now, now)
-mycursor.execute(sql, val)
+# sql = "INSERT INTO polls (bidenPercent, trumpPercent, date, pollster, createdAt, updatedAt) VALUES (%s, %s, %s, %s, %s, %s)"
+# # these vaues will be our variable from below - this will prob go after the scraping stuff
+# val = (39, 61, now, "uGov", now, now)
+# mycursor.execute(sql, val)
 
-mydb.commit()
+# mydb.commit()
 
 
 # set up soup
@@ -37,21 +36,27 @@ for date in dates:
     print(line1.text)
 
 # grabs pollster names
+allPollsters = []
 pollsters = soup.find_all("td", class_="pollster")
 for p in pollsters:
+    allPollsters.append(p.text)
     print(p.text)
+print("allPollsters", allPollsters)
 
 # gets all poll numbers tds (with names and numbers)
 numbers = soup.find_all("td", class_="answers hide-desktop")
 # gets all just biden numbers
+allBiden = []
 for n in numbers:
     biden = n.find_all("div")[2]
-    print("Biden", biden.text)
-
+    allBiden.append(biden.text)
+print(allBiden)
 # gets all just trump numbers
+allTrump = []
 for n in numbers:
     trump = n.find_all("div")[4]
-    print("Trump", trump.text)
+    allTrump.append(trump.text)
+print(allTrump)
 
 
 
